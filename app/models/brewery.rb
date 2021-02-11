27dd77196c915a468f165d84b25baa7600a1fb7f
Zach <zbayard@gmail.com>
2021-02-11 17:42:58 -0500

    models

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,7 +1,7 @@
 class Brewery < ApplicationRecord
     
     has_many :reviews 
-    has many :users, through: :reviews
+    has_many :users, through: :reviews
     has_many :beers 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :reviews
-  resources :beers
   resources :breweries
+  resources :beers
+  
   # resources :users
 
   post '/login', to: 'user#login'
